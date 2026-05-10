4288c2106c28a1e0101d8cb9c133579fdc51fa09
HaoChen_07 <123456789haoch@gmail.com>
2026-05-10 17:57:46 +0200

    Algo de boton play

---
+++ modelo/objetos/BotonPlay.java
@@ -0,0 +1,24 @@
+package modelo.objetos;
+import modelo.base.*;
+
+public class BotonPlay extends Boton {
+
+    public BotonPlay(String n, double x, double y) {
+        super(n, x, y);
+    }
+
+    @Override
+    public void pintar() {
+        // Código para dibujar el botón de play
+    }
+
+    @Override
+    public void mouse() {
+        // Código para detectar el clic del mouse en el botón
+    }
+
+    @Override
+    public void interaccion() {
+        // Código para iniciar el juego cuando se haga clic en el botón
+    }
+}
